8f5d31e56f24dc0103c7cf1fa47e32a92f5ef53c
Christopher <chrisabbod@gmail.com>
2022-08-28 18:16:27 -0400

    Use viewbinding to inflate fragment view

--- app/src/main/java/com/chrisabbod/wordscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/chrisabbod/wordscramble/ui/game/GameFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.chrisabbod.wordscramble.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.chrisabbod.wordscramble.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
